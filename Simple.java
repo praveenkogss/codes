177805c202432452a4fb030a13345d113a70b29e
praveenkogss <ogss.praveenk@gmail.com>
2022-03-19 18:06:41 +0530

    My commit

--- Simple.java
+++ Simple.java
@@ -3,13 +3,8 @@ class Simple{
      System.out.println("Hello Java");  
         System.out.println("First Commit");  
         System.out.println("second Commit"); 
-        System.out.println("Third Commit");
-        
-        System.out.println("4 Commit"); 
-        System.out.println("5 Commit"); 
-        System.out.println("5 Commit"); 
-        System.out.println("5 Commit"); 
-        
+       
+       
         System.out.println("5 Commit"); System.out.println("5 Commit"); 
          System.out.println("5 Commit"); System.out.println("5 Commit"); 
         
