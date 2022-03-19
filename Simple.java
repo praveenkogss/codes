42c08fa584000e87fe24637d1f5f0af724ba2224
praveenkogss <ogss.praveenk@gmail.com>
2022-03-19 18:10:16 +0530

    My Commitsdf

--- Simple.java
+++ Simple.java
@@ -1,13 +1,8 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-        System.out.println("First Commit");  
-        System.out.println("second Commit"); 
-       
-       
-        System.out.println("5 Commit"); System.out.println("5 Commit"); 
-         System.out.println("5 Commit"); System.out.println("5 Commit"); 
+      
         
-        System.out.println("5 Commit"); System.out.println("5 Commit");
+        System.out.println("5 Commit"); System.out.println("5 Commdasit");
     }  
 } 
