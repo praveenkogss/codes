ca13d556bd54279e1d28b8e042536fc314480129
praveenkogss <94841726+praveenkogss@users.noreply.github.com>
2022-03-19 12:51:15 +0530

    Third Commit
    Third Commit

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
         System.out.println("First Commit");  
-        System.out.println("second Commit");  
+        System.out.println("second Commit"); 
+        System.out.println("Third Commit"); 
     }  
 } 
